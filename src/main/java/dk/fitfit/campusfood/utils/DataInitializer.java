9316bfd571332ae439f7a255194078d22209fafa
Sebastian Jensen <sebastianthegreatful@gmail.com>
2014-10-19 04:10:18 +0200

    Added more test data.

--- src/main/java/dk/fitfit/campusfood/utils/DataInitializer.java
+++ src/main/java/dk/fitfit/campusfood/utils/DataInitializer.java
@@ -28,16 +28,38 @@ public class DataInitializer {
 		String contact = "Kontakt... someone.";
 		Canteen canteen = new Canteen(name, address, openingHours, contact);
 
+		name = "Kantinen i August Krogh Bygningen";
+		address = "Somewhere 4";
+		openingHours = "Mandag til torsdag 8:00 - 15:00. Fredag 8:00 - 14:30\nMorgenmad 8:00 - 9:30\nFrokost 11:00 - 14:00";
+		contact = "Kontakt... someone.";
+		Canteen canteen1 = new Canteen(name, address, openingHours, contact);
+
 		Meal meal = new Meal();
-		meal.setName("Pasta");
+		meal.setName("Braiseret skinke culotte");
 		meal.setDateOfServing(new Date());
 		canteen.addMeal(meal);
 
 		Meal meal1 = new Meal();
-		meal1.setName("Pasta something");
+		meal1.setName("Kylling i sur sød med bambus");
 		meal1.setDateOfServing(new Date());
 		canteen.addMeal(meal1);
+		
+		Meal meal2 = new Meal();
+		meal2.setName("Fiske deller med spinat");
+		meal2.setDateOfServing(new Date());
+		canteen.addMeal(meal2);
+
+		Meal meal3 = new Meal();
+		meal3.setName("Chili con carne");
+		meal3.setDateOfServing(new Date());
+		canteen1.addMeal(meal3);
+
+		Meal meal4 = new Meal();
+		meal4.setName("Kalkun stegt med citron");
+		meal4.setDateOfServing(new Date());
+		canteen1.addMeal(meal4);
 
 		Canteen c = canteenRepository.save(canteen);
+		Canteen c1 = canteenRepository.save(canteen1);
 	}
 }
