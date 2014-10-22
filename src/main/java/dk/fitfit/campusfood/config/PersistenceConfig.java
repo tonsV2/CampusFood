282160e7e64889e0556173f01b13b1cfb7890c7b
Sebastian Jensen <sebastianthegreatful@gmail.com>
2014-10-22 21:40:49 +0200

    Tried to use a datasource which allowed me to connect through eclipse data source explorer... didn't succeed. I left the new datasource commented out for futura reference.

--- src/main/java/dk/fitfit/campusfood/config/PersistenceConfig.java
+++ src/main/java/dk/fitfit/campusfood/config/PersistenceConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -51,9 +52,20 @@ public class PersistenceConfig {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
+//	@Bean
+//	public DataSource dataSource() {
+//		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+//	}
+
 	@Bean
-	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+	public DataSource dataSource()
+	{
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("sa");
+		dataSource.setDriverClassName("org.h2.Driver");
+		return dataSource;
 	}
 
 	@Bean
