ea239d1e6179c4f7eb7e1cdff65d3382039a1c25
Sebastian Jensen <sebastianthegreatful@gmail.com>
2014-11-06 22:16:39 +0100

    Added mealById to api controller.

--- src/main/java/dk/fitfit/campusfood/controller/ApiController.java
+++ src/main/java/dk/fitfit/campusfood/controller/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import dk.fitfit.campusfood.model.Meal;
@@ -41,6 +42,11 @@ public class ApiController {
 	@RequestMapping(value = "/meals/week/{week}")
 	public List<Meal> mealsByWeek(@PathVariable int week) {
 		return mealService.findMealsByWeek(week);
+	}
+
+	@RequestMapping(value = "/meal/{id}")
+	public Meal mealById(@PathVariable long id) {
+		return mealService.findOne(id);
 	}
 
 }
